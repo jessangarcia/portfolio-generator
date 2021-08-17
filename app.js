5835ca8d19011b70a70fd803a417473b7e1ce83c
jessan <jessenia.garcia998@gmail.com>
2021-08-17 03:09:47 -0600

    questions and console logs for the readme template

--- app.js
+++ app.js
@@ -13,53 +13,52 @@ const generatePage = require("./src/page-template")
 
 
 const promptUser = () => {
-    return inquirer
-        .prompt([
-            {
-                type: 'input',
-                name: 'name',
-                message: 'What is your name?',
-                validate: nameInput => {
-                    if (nameInput){
-                        return true;
-                    } else {
-                        console.log('Please enter your name!');
-                        return false;
-                    }
-                }
-            },
-            {
-                type: 'input',
-                name: 'github',
-                message: 'Enter your GitHub Username',
-                validate: gitName => {
-                    if (gitName){
-                        return true;
-                    } else {
-                        console.log('Please enter your name!');
-                        return false;
-                    }
-                }
-            },
-            {
-                type: 'confirm',
-                name: 'confirmAbout',
-                message: 'Would you like to enter some information about yourself for an "About" section?',
-                default: true
-            },
-            {
-                type: 'input',
-                name: 'about',
-                message: 'Provide some information about yourself',
-                when: ({confirmAbout}) => {
-                    if (confirmAbout) {
-                        return true;
-                    } else {
-                        return false;
-                    }
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+            validate: nameInput => {
+                if (nameInput){
+                    return true;
+                } else {
+                    console.log('Please enter your name!');
+                    return false;
                 }
             }
-        ]);
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub Username',
+            validate: gitName => {
+                if (gitName){
+                    return true;
+                } else {
+                    console.log('Please enter your name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would you like to enter some information about yourself for an "About" section?',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself',
+            when: ({confirmAbout}) => {
+                if (confirmAbout) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+    ]);
 };
 
 const promptProject = portfolioData => {
